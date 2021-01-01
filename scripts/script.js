e92732c146d9b58a9984b916fcb5fbb8bfda98ab
Ryan Kim <hanjun1@ualberta.ca>
2020-12-31 19:10:22 -0700

    added basic html and reset stylesheet

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,33 @@
 function add(num1, num2) {
-    return num1 + num2;
+  return num1 + num2;
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+  return num1 - num2;
 }
 
 function multiply(num1, num2) {
-    return num1 * num2; 
+  return num1 * num2;
 }
 
 function divide(num1, num2) {
-    if (num2 === 0) {
-        return infinity;
-    }
-    return num1 / num2;
+  if (num2 === 0) {
+    return infinity;
+  }
+  return num1 / num2;
 }
 
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "add":
+      return add(num1, num2);
+    case "subract":
+      return sub(num1, num2);
+    case "multiply":
+      return multiply(num1, num2);
+    case "divide":
+      return divide(num1, num2);
+    default:
+      return "Something has gone terribly wrong!";
+  }
+}
